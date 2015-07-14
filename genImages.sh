a856ccd1cd419b0d8ba07aafceefeffc394411d2
Olivier Fayau <olivier.fayau@gmail.com>
2015-07-14 22:39:26 +0200

    Commit de j2me

---
+++ genImages.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Version
+ejdk_zip=ojec-1.1.1-rr-bin-b02-linux-x86-generic-product-22_apr_2013.zip
+ejdk=ojec1.1.1
+
+# unzip ejdk downloaded from Oracle.com
+unzip -o $ejdk_zip -x index*
+
+# Build docker image
+docker build -t "java-me" -f src_gen/Dockerfile .
+
+# Clean unzip jdk
+rm -rf $ejdk
+
+echo "Image were builded OK"
+
